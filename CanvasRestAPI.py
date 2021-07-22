51050d5531d075b19c39c2496c73d84f12a75a21
cthacker-udel <cthacker@udel.edu>
2021-07-22 04:09:10 -0400

    Added : Account Notifications API : Show Global Notification

--- CanvasRestAPI.py
+++ CanvasRestAPI.py
@@ -89,6 +89,14 @@ class CanvasAccountNotifications(CanvasClient):
 
         pprint(request)
 
+    def show_global_notification(self):
+
+        url = self.base_url + '/api/v1/accounts/{}/account_notifications/{}'.format(self.client.CanvasAccountNotifications.account_id,self.client.CanvasAccountNotifications.account_notification_id)
+
+        request = requests.get(url,headers={'Authorization {}'.format(self.token)})
+
+        pprint(request)
+
 
 
 
