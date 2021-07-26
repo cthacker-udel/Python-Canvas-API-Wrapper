214af89275bbe3820d5a67d6b788807763624951
cthacker-udel <cthacker@udel.edu>
2021-07-25 21:26:20 -0400

    Added : Analytics API : Get user participation data

--- CanvasRestAPI.py
+++ CanvasRestAPI.py
@@ -417,5 +417,13 @@ class CanvasAnalytics(CanvasClient):
 
         pprint(request)
 
+    def get_user_participation_data(self):
+
+        url = self.base_url + '/api/v1/courses/{}/analytics/users/{}/activity'.format(self.client.CanvasAnalytics.course_id,self.client.CanvasAnalytics.student_id)
+
+        request = requests.get(url,headers={'Authorization {}'.format(self.token)})
+
+        pprint(request)
+
 
 
