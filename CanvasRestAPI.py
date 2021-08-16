e94d10b2e443d72d7350c865ded97395e5cc60f2
cthacker-udel <cthacker@udel.edu>
2021-08-16 12:14:03 -0400

    Added : Authorization Providers API : delete authentication provider

--- CanvasRestAPI.py
+++ CanvasRestAPI.py
@@ -843,6 +843,16 @@ class CanvasAuthenticationProviders(CanvasClient):
 
         pprint(request)
 
+    def delete_authentication_provider(self):
+
+        url = self.base_url + '/api/v1/accounts/{}/authentication_providers/{}'.format(self.client.CanvasAuthProvider.account_id,self.client.CanvasAuthProvider.authentication_provider_id)
+
+        request = requests.delete(url,headers={'Authorization {}'.format(self.token)},json=self.client.CanvasAuthProvider.generate_queries())
+
+        pprint(request)
+
+
+
 
 
 
