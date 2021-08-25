f997f606d39ff1cf87b80c32d09bd2119f3dd953
cthacker-udel <cthacker@udel.edu>
2021-08-25 05:21:55 -0400

    Added : Content Migration API : list content migrations group id

--- CanvasRestAPI.py
+++ CanvasRestAPI.py
@@ -1493,6 +1493,14 @@ class CanvasContentMigration(CanvasClient):
 
         pprint(request)
 
+    def list_content_migrations_group_id(self):
+
+        url = self.base_url + '/api/v1/groups/{}/content_migrations'.format(self.client.CanvasContentMigration.group_id)
+
+        request = requests.get(url,headers={'Authorization {}'.format(self.token)})
+
+        pprint(request)
+
 
 
 
