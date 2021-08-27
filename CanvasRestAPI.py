e8b306b10e9b7d8caa7bbde6218577f31e9845c0
cthacker-udel <cthacker@udel.edu>
2021-08-27 06:06:08 -0400

    Added : Content Migration API : get content migration account id

--- CanvasRestAPI.py
+++ CanvasRestAPI.py
@@ -1629,6 +1629,13 @@ class CanvasContentMigration(CanvasClient):
 
         pprint(request)
 
+    def get_content_migration_account_id(self):
+
+        url = self.base_url + '/api/v1/accounts/{}/content_migrations/{}'.format(self.client.CanvasContentMigration.account_id)
+
+        request = requests.get(url,headers={'Authorization {}'.format(self.token)})
+
+        pprint(request)
 
 
 
